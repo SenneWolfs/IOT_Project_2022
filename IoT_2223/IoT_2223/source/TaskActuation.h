ddc86454a4241a496a53348476b336f1a9567b92
i5cP7i <48355782+i5cP7i@users.noreply.github.com>
2023-01-03 14:03:25 +0100

    Update IoT Project
    Finished IoT project.

---
+++ IoT_2223/IoT_2223/source/TaskActuation.h
@@ -0,0 +1,36 @@
+/*
+ * TaskActuation.h
+ *
+ *  Created on: Dec 09, 2022
+ *      Author: Eduardo Bemelmans
+ */
+
+#ifndef TASKACTUATION_H_
+#define TASKACTUATION_H_
+
+/*******************************************************************************
+ * Header file includes
+ ******************************************************************************/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "ActuationData.h"
+#include "timers.h"
+
+
+/*******************************************************************************
+ * Function prototype
+ ******************************************************************************/
+void TaskActuation(void *arg);
+
+/* Actuation Client task handle. */
+extern TaskHandle_t TaskActuationHandle;
+extern QueueHandle_t queue_actuation_handle_pld;
+extern QueueHandle_t queue_actuation_handle_servo;
+extern TimerHandle_t timer_handle_actuation;
+extern actuation_data_msg_t actuation_data;
+
+extern QueueHandle_t queue_speed_handle;
+extern QueueHandle_t queue_servo_handle;
+
+#endif /* TASKACTUATION_H_ */
---
+++ IoT_2223/IoT_2223/source/publisher_task.c
@@ -0,0 +1,342 @@
+/******************************************************************************
+* File Name:   publisher_task.c
+*
+* Description: This file contains the task that sets up the user button GPIO 
+*              for the publisher and publishes MQTT messages on the topic
+*              'MQTT_PUB_TOPIC' to control a device that is actuated by the
+*              subscriber task. The file also contains the ISR that notifies
+*              the publisher task about the new device state to be published.
+*
+* Related Document: See README.md
+*
+*
+*******************************************************************************
+* Copyright 2020-2022, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
+#include "cy_pdl.h"
+#include "cyhal.h"
+#include "cybsp.h"
+#include "FreeRTOS.h"
+
+/* Task header files */
+#include "publisher_task.h"
+#include "mqtt_task.h"
+#include "TaskActuation.h"
+
+/* Configuration file for MQTT client */
+#include "mqtt_client_config.h"
+
+/* Middleware libraries */
+#include "cy_mqtt_api.h"
+#include "cy_retarget_io.h"
+
+#include <stdio.h>
+#include <string.h>
+
+/******************************************************************************
+* Macros
+******************************************************************************/
+/* Interrupt priority for User Button Input. */
+#define USER_BTN_INTR_PRIORITY          (3)
+
+/* The maximum number of times each PUBLISH in this example will be retried. */
+#define PUBLISH_RETRY_LIMIT             (10)
+
+/* A PUBLISH message is retried if no response is received within this 
+ * time (in milliseconds).
+ */
+#define PUBLISH_RETRY_MS                (1000)
+
+/* Queue length of a message queue that is used to communicate with the 
+ * publisher task.
+ */
+#define PUBLISHER_TASK_QUEUE_LENGTH     (3u)
+
+/* The input message size (inclusive of the string terminating character '\0').
+ * Edit this macro to suit your message size.
+ */
+#define MAX_MESSAGE_SIZE                     (100u)
+
+/* Size of the message block that can be processed by Crypto hardware for
+ * AES encryption.
+ */
+#define AES128_ENCRYPTION_LENGTH             (uint32_t)(16u)
+
+#define AES128_KEY_LENGTH                    (uint32_t)(16u)
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+static void publisher_init(void);
+static void publisher_deinit(void);
+static void isr_button_press(void *callback_arg, cyhal_gpio_event_t event);
+void print_heap_usage(char *msg);
+void encrypt_message(uint8_t* message, uint8_t size);
+
+/******************************************************************************
+* Global Variables
+*******************************************************************************/
+/* FreeRTOS task handle for this task. */
+TaskHandle_t publisher_task_handle;
+
+/* Handle of the queue holding the commands for the publisher task */
+QueueHandle_t publisher_task_q;
+
+/* Structure to store publish message information. */
+cy_mqtt_publish_info_t publish_info =
+{
+    .qos = (cy_mqtt_qos_t) MQTT_MESSAGES_QOS,
+    .topic = MQTT_PUB_TOPIC,
+    .topic_len = (sizeof(MQTT_PUB_TOPIC) - 1),
+    .retain = false,
+    .dup = false
+};
+
+/* Structure that stores the callback data for the GPIO interrupt event. */
+cyhal_gpio_callback_data_t cb_data =
+{
+    .callback = isr_button_press,
+    .callback_arg = NULL
+};
+
+
+/* Variables to hold the user message and the corresponding encrypted message */
+CY_ALIGN(4) uint8_t message[MAX_MESSAGE_SIZE];
+CY_ALIGN(4) uint8_t encrypted_msg[MAX_MESSAGE_SIZE];
+
+cy_stc_crypto_aes_state_t aes_state;
+
+/* Key used for AES encryption*/
+// abcdefghijklmnop
+CY_ALIGN(4) uint8_t aes_key[AES128_KEY_LENGTH] = {0x61, 0x62, 0x63, 0x64,
+                                                  0x65, 0x66, 0x67, 0x68,
+                                                  0x69, 0x6A, 0x6B, 0x6C,
+                                                  0x6D, 0x6E, 0x6F, 0x70,};
+// 1234567890123456
+uint8_t iv[AES128_KEY_LENGTH] = {0x31, 0x32, 0x33, 0x34,
+                                 0x35, 0x36, 0x37, 0x38,
+                                 0x39, 0x30, 0x31, 0x32,
+                                 0x33, 0x34, 0x35, 0x36,};
+uint8_t aes_block_count = 0;
+
+/******************************************************************************
+ * Function Name: publisher_task
+ ******************************************************************************
+ * Summary:
+ *  Task that sets up the user button GPIO for the publisher and publishes 
+ *  MQTT messages to the broker. The user button init and deinit operations,
+ *  and the MQTT publish operation is performed based on commands sent by other
+ *  tasks and callbacks over a message queue.
+ *
+ * Parameters:
+ *  void *pvParameters : Task parameter defined during task creation (unused)
+ *
+ * Return:
+ *  void
+ *
+ ******************************************************************************/
+void publisher_task(void *pvParameters)
+{
+    /* Status variable */
+    cy_rslt_t result;
+
+    publisher_data_t publisher_q_data;
+
+    char json[64];
+    //uint8_t json_uint[64];
+    //char encrypted[MAX_MESSAGE_SIZE];
+    //static char hex[] = "0123456789ABCDEF";
+
+    int val_bat;
+    int val_speed;
+    int val_serv;
+
+    /* Command to the MQTT client task */
+    mqtt_task_cmd_t mqtt_task_cmd;
+
+    /* To avoid compiler warnings */
+    (void) pvParameters;
+
+    /* Initialize and set-up the user button GPIO. */
+    publisher_init();
+
+    /* Create a message queue to communicate with other tasks and callbacks. */
+    publisher_task_q = xQueueCreate(PUBLISHER_TASK_QUEUE_LENGTH, sizeof(publisher_data_t));
+
+    Cy_Crypto_Core_Enable(CRYPTO);
+
+    while (true)
+    {
+    	xQueueReceive(queue_battery_handle, &val_bat, portMAX_DELAY );
+    	xQueueReceive(queue_speed_handle, &val_speed, portMAX_DELAY );
+    	xQueueReceive(queue_servo_handle, &val_serv, portMAX_DELAY );
+        	sprintf(json, "{\"batteryLevel\":%d,\"Speed\":%d,\"Servo\":%d}", val_bat, val_speed, val_serv); //Met security: 0000000
+        	/*for (int i = 0; i<sizeof(json); i++) {
+        		json_uint[i] = (uint8_t)(json[i]);
+        		printf("%02x ", json_uint[i]);
+        	}
+        	printf("\r\n");
+        	encrypt_message(json_uint, sizeof(json));
+
+        	for (size_t i = 0; i < (aes_block_count * AES128_ENCRYPTION_LENGTH); i++)
+        	{
+        		encrypted[(i * 2) + 0] = hex[((encrypted_msg[i] & 0xF0) >> 4)];
+        		encrypted[(i * 2) + 1] = hex[((encrypted_msg[i] & 0x0F) >> 0)];
+        	}
+        	printf("Encrypted: %s \r\n", encrypted);*/
+        	/* Publish the data received over the message queue. */
+        	publish_info.payload = json;
+        	publish_info.payload_len = strlen(json);
+
+        	result = cy_mqtt_publish(mqtt_connection, &publish_info);
+
+        	if (result != CY_RSLT_SUCCESS)
+        	{
+        		printf("  Publisher: MQTT Publish failed with error 0x%0X.\n\n", (int)result);
+
+        	    /* Communicate the publish failure with the the MQTT
+        	    * client task.
+        	    */
+        	    mqtt_task_cmd = HANDLE_MQTT_PUBLISH_FAILURE;
+        	    xQueueSend(mqtt_task_q, &mqtt_task_cmd, portMAX_DELAY);
+        	}
+
+        	print_heap_usage("publisher_task: After publishing an MQTT message");
+    }
+}
+
+void encrypt_message(uint8_t* message, uint8_t size)
+{
+    aes_block_count =  (size % AES128_ENCRYPTION_LENGTH == 0) ?
+                       (size / AES128_ENCRYPTION_LENGTH)
+                       : (1 + size / AES128_ENCRYPTION_LENGTH);
+
+    /* Initializes the AES operation by setting key and key length */
+    Cy_Crypto_Core_Aes_Init(CRYPTO, aes_key, CY_CRYPTO_KEY_AES_128, &aes_state);
+
+    for (int i = 0; i < aes_block_count ; i++)
+    {
+        /* Perform AES CBC Encryption mode of operation */
+        Cy_Crypto_Core_Aes_Cbc(CRYPTO, CY_CRYPTO_ENCRYPT, (aes_block_count*AES128_ENCRYPTION_LENGTH), iv, (encrypted_msg + AES128_ENCRYPTION_LENGTH * i), (message + AES128_ENCRYPTION_LENGTH * i), &aes_state);
+
+        /* Wait for Crypto Block to be available */
+        Cy_Crypto_Core_WaitForReady(CRYPTO);
+    }
+
+    Cy_Crypto_Core_Aes_Free(CRYPTO, &aes_state);
+}
+
+/******************************************************************************
+ * Function Name: publisher_init
+ ******************************************************************************
+ * Summary:
+ *  Function that initializes and sets-up the user button GPIO pin along with  
+ *  its interrupt.
+ * 
+ * Parameters:
+ *  void
+ *
+ * Return:
+ *  void
+ *
+ ******************************************************************************/
+static void publisher_init(void)
+{
+    /* Initialize the user button GPIO and register interrupt on falling edge. */
+    cyhal_gpio_init(CYBSP_USER_BTN, CYHAL_GPIO_DIR_INPUT,
+                    CYHAL_GPIO_DRIVE_PULLUP, CYBSP_BTN_OFF);
+    cyhal_gpio_register_callback(CYBSP_USER_BTN, &cb_data);
+    cyhal_gpio_enable_event(CYBSP_USER_BTN, CYHAL_GPIO_IRQ_FALL,
+                            USER_BTN_INTR_PRIORITY, true);
+    
+    printf("Press the user button (SW2) to publish \"%s\"/\"%s\" on the topic '%s'...\n\n", 
+           MQTT_DEVICE_ON_MESSAGE, MQTT_DEVICE_OFF_MESSAGE, publish_info.topic);
+}
+
+/******************************************************************************
+ * Function Name: publisher_deinit
+ ******************************************************************************
+ * Summary:
+ *  Cleanup function for the publisher task that disables the user button  
+ *  interrupt and deinits the user button GPIO pin.
+ *
+ * Parameters:
+ *  void
+ *
+ * Return:
+ *  void
+ *
+ ******************************************************************************/
+static void publisher_deinit(void)
+{
+    /* Deregister the ISR and disable the interrupt on the user button. */
+    cyhal_gpio_register_callback(CYBSP_USER_BTN, &cb_data);
+    cyhal_gpio_enable_event(CYBSP_USER_BTN, CYHAL_GPIO_IRQ_FALL,
+                            USER_BTN_INTR_PRIORITY, false);
+    cyhal_gpio_free(CYBSP_USER_BTN);
+}
+
+/******************************************************************************
+ * Function Name: isr_button_press
+ ******************************************************************************
+ * Summary:
+ *  GPIO interrupt service routine. This function detects button
+ *  presses and sends the publish command along with the data to be published 
+ *  to the publisher task over a message queue. Based on the current device 
+ *  state, the publish data is set so that the device state gets toggled.
+ *
+ * Parameters:
+ *  void *callback_arg : pointer to variable passed to the ISR (unused)
+ *  cyhal_gpio_event_t event : GPIO event type (unused)
+ *
+ * Return:
+ *  void
+ *
+ ******************************************************************************/
+static void isr_button_press(void *callback_arg, cyhal_gpio_event_t event)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    publisher_data_t publisher_q_data;
+
+    /* To avoid compiler warnings */
+    (void) callback_arg;
+    (void) event;
+
+    /* Assign the publish command to be sent to the publisher task. */
+    publisher_q_data.cmd = PUBLISH_MQTT_MSG;
+
+    /* Send the command and data to publisher task over the queue */
+    xQueueSendFromISR(publisher_task_q, &publisher_q_data, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+/* [] END OF FILE */
